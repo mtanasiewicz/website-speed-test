4e1aaa0dc937f99198eaab5dbc161644d7632ab0
Marcin Tanasiewicz <mtanasiewicz@users.noreply.github.com>
2019-06-14 11:55:42 +0200

    add autowiring and annotations configs

--- src/Benchmark/Ui/Http/Rest/BenchmarkController.php
+++ src/Benchmark/Ui/Http/Rest/BenchmarkController.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 namespace App\Benchmark\Ui\Http\Rest;
 
 use App\Shared\Ui\Http\Rest\RestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class BenchmarkController
+ * @package App\Benchmark\Ui\Http\Rest
+ *
+ * @Rest\Route("/api")
+ */
 class BenchmarkController extends RestController
 {
+    /**
+     * @Rest\Route(path="/benchmark", methods={"GET"}, name="api-benchmark-create")
+     *
+     * @return Response
+     */
+    public function createBenchmark(): Response
+    {
 
+    }
 }
